24ff9dfc5db4b7f9e4075424b0c5d30fe4814f66
J-Segers <jethroseger@gmail.com>
2022-08-26 21:06:47 +0200

    created the footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div id={"page-footer-container"}>
+            <label>© 2022 - Jethro Segers</label>
+        </div>
+    );
+}
+
+export default Footer;
