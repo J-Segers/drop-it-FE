648ea0d946f0237ef7505852a1d0e46797b7c103
J-Segers <jethroseger@gmail.com>
2023-07-20 11:35:26 +0200

    code musicplayer opgeschoond

--- src/components/musicPlayer/MusicPlayer.js
+++ src/components/musicPlayer/MusicPlayer.js
@@ -4,10 +4,9 @@ import {MusicPlayerContext} from "../../context/MusicPlayerProvider";
 import Visualizer from "./subComponents/visualizer/Visualizer";
 import PlayList from "./subComponents/playlist/PlayList";
 import ControlPanel from "./subComponents/controlPanel/ControlPanel";
-// import liedje from "../../assets/Marc Anthony - Volver a Comenzar (3.0) (Salsa 2013) (Album 2013).mp3";
 
 function MusicPlayer() {
-    const {isLargePlayer, audioElement, currentSong, isPlaying} = useContext(MusicPlayerContext);
+    const {isLargePlayer, audioElement, currentSong, isPlaying, nextSong} = useContext(MusicPlayerContext);
 
     useEffect(() => {
         if(isPlaying){
@@ -15,12 +14,18 @@ function MusicPlayer() {
         }
     },[currentSong]);
 
+    useEffect(() => {
+        if(audioElement.current.currentTime > audioElement.current.duration) {
+            nextSong();
+        }
+    })
+
     return (
         <div id={isLargePlayer ? "large-player" : "compact-player"}>
             {isLargePlayer && <Visualizer />}
             {isLargePlayer && <PlayList />}
             <ControlPanel />
-            <audio src={currentSong?.song?.url} preload={"metadata"} ref={audioElement} onTimeUpdate={ontimeupdate}/>
+            <audio src={currentSong?.song?.url} preload={"metadata"} ref={audioElement} onTimeUpdate={ontimeupdate} />
         </div>
     );
 }
--- src/components/musicPlayer/subComponents/display/Display.js
+++ src/components/musicPlayer/subComponents/display/Display.js
@@ -1,33 +1,46 @@
 import React, {useContext, useEffect, useRef, useState} from 'react';
 import "./Display.css";
 import {MusicPlayerContext} from "../../../../context/MusicPlayerProvider";
+import getTimeStampFromDuration from "../../../../helperFuncties/convertLengthToTime";
 
 function Display() {
 
-    const {isPlaying, currentSong, audioElement} = useContext(MusicPlayerContext);
+    const {isPlaying, currentSong, audioElement, nextSong} = useContext(MusicPlayerContext);
     const [currentSongInfo, setCurrentSongInfo] = useState("test");
 
     const [min, setMin] = useState("0");
-    const [sec, setSec] = useState("0");
+    const [sec, setSec] = useState("00");
     const [curMin, setCurMin] = useState("0");
-    const [curSec, setCurSec] = useState("0");
+    const [curSec, setCurSec] = useState("00");
 
     const progressBar = useRef();
 
     useEffect(() => {
+        if(audioElement.current.currentTime > audioElement.current.duration - 1) {
+            nextSong();
+        }
+    });
 
-        if(currentSong != null) {
-            progressBar.current.max = Math.floor(audioElement.current.duration);
-            let newSec = Math.floor(audioElement.current.duration % 60);
-            setMin(`${Math.floor(audioElement.current.duration / 60)}`);
-            if (newSec < 10) {
-                setSec(`0${newSec}`);
-            } else {
-                setSec(`${newSec}`);
+    useEffect(() => {
+
+        let duration = audioElement?.current?.duration;
+
+        if(duration > 0) {
+            progressBar.current.max = Math.floor(duration) - 1;
+
+            // array [minutes, seconds]
+            let timeStampArray = getTimeStampFromDuration(duration);
+            if(timeStampArray.length > 0) {
+                setMin(timeStampArray[0]);
+                if (timeStampArray[1] < 10) {
+                    setSec(`0${timeStampArray[1]}`);
+                } else {
+                    setSec(`${timeStampArray[1]}`);
+                }
             }
+
             setCurrentSongInfo(`${currentSong.title} - ${currentSong.artist}`);
         }
-
 
     });
 
@@ -42,6 +55,7 @@ function Display() {
                 } else {
                     setCurSec(`${newCurSec}`);
                 }
+
                 progressBar.current.value = audioElement.current.currentTime;
 
             },100);
